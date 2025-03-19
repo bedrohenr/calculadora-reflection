2bf953241f6d09c438496ad339bf694719260ff0
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-18 21:05:46 -0300

    Inserindo o menu do projeto calculadora mvc

--- src/main/java/org/example/application/Main.java
+++ src/main/java/org/example/application/Main.java
@@ -1,11 +1,17 @@
 package org.example.application;
 
 import org.example.view.Menu;
+import org.example.controller.ControllerCalc;
+import org.example.dto.RequestDTO;
+import org.example.dto.ResponseDTO;
 
 public class Main {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        RequestDTO requestDTO = menu.show();
+        ControllerCalc controllerCalc = new ControllerCalc();
+        ResponseDTO responseDTO = controllerCalc.calc(requestDTO);
+        menu.showResult(responseDTO);
 
-
-        Menu.show();
     }
 }
