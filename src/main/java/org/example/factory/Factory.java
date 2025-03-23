42ba248c90e7b5b26c7840b6caab8c8ddd7f9c4b
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-23 18:52:24 -0300

    Classe Factory, com metodo para criar uma classe usando seu nome

---
+++ src/main/java/org/example/factory/Factory.java
@@ -0,0 +1,16 @@
+package org.example.factory;
+
+import java.lang.reflect.Constructor;
+
+public class Factory {
+    public static Object createInstance(String className) throws Exception {
+        String module = "org.example.model.operation.";
+        // Carrega a classe com o nome dela
+        Class<?> clazz = Class.forName(module + className);
+
+        // Obtem o construtor padrao, sem argumentos
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+
+        return constructor.newInstance();
+    }
+}
