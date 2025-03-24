672d6e4921bb2f289c755ffc19f29066209e9b36
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-24 10:31:13 -0300

    Prototype classe somar

--- src/main/java/org/example/model/operation/Somar.java
+++ src/main/java/org/example/model/operation/Somar.java
@@ -5,4 +5,12 @@ public class Somar implements IOperation{
     public int calc (int value1, int value2){
         return value1 + value2;
     }
+
+    public Somar clone() {
+        try {
+            return (Somar) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        } 
+    }
 }
