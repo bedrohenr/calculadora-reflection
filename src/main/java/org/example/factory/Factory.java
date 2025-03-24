1679dd2549862bb9d1f9397feca178e5d4c33177
Pedro Henrique Fonseca Ferreira <20231BSI0503@cefetes.br>
2025-03-24 09:49:02 -0300

    adicionando o singleton na Factory

--- src/main/java/org/example/factory/Factory.java
+++ src/main/java/org/example/factory/Factory.java
@@ -3,6 +3,18 @@ package org.example.factory;
 import java.lang.reflect.Constructor;
 
 public class Factory {
+    private static Factory instance;
+    
+    private Factory () {}
+
+    public static Factory getInstance () {
+        if(Factory.instance == null) {
+            Factory.instance = new Factory();
+        }
+
+        return instance;
+    }
+
     public static Object getIOperationClassInstance(String className) throws Exception {
         String module = "org.example.model.operation.";
         // Carrega a classe com o nome dela
