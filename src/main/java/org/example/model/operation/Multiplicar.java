a4e5b07a557e85351b44d58b37e43d859a9508cc
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-24 10:30:28 -0300

    prototype classe Multiplicar

--- src/main/java/org/example/model/operation/Multiplicar.java
+++ src/main/java/org/example/model/operation/Multiplicar.java
@@ -5,4 +5,12 @@ public class Multiplicar implements IOperation{
     public int calc (int value1, int value2){
         return value1 * value2;
     }
+
+    public Multiplicar clone() {
+        try {
+            return (Multiplicar) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        } 
+    }
 }
