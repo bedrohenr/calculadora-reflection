4a3960855b0585484e6d14d172db24c64277f57d
Pedro Ferreira <ferfonhp@gmail.com>
2025-03-24 10:31:28 -0300

    Prototype classe subtrair

--- src/main/java/org/example/model/operation/Subtrair.java
+++ src/main/java/org/example/model/operation/Subtrair.java
@@ -5,4 +5,12 @@ public class Subtrair implements IOperation{
     public int calc (int value1, int value2){
         return value1 - value2;
     }
+
+    public Subtrair clone() {
+        try {
+            return (Subtrair) super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new AssertionError();
+        } 
+    }
 }
